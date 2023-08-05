537e726fe54f3126c70c630558ba3b47aa824ed5
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-08-05 18:01:38 +0400

    feat: made UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import Paper from "./components/SVG/Paper";
+import Rock from "./components/SVG/Rock";
+import Scissors from "./components/SVG/Scissors";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const RockPaperScissors = () => {
+  return (
+    <main className="game-container">
+      <header>
+        <h1>Rock Paper Scissors</h1>
+      </header>
+      <section className="player-container">
+        <h2>Player</h2>
+        <span className="far" id="playerRock">
+          <Rock />
+        </span>
+        <span className="far" id="playerPaper">
+          <Paper />
+        </span>
+        <span className="far" id="playerScissors">
+          <Scissors />
+        </span>
+      </section>
+      <section className="computer-container">
+        <h2>Computer</h2>
+        <span className="far" id="computerRock">
+          <Rock />
+        </span>
+        <span className="far" id="computerPaper">
+          <Paper />
+        </span>
+        <span className="far" id="computerScissors">
+          <Scissors />
+        </span>
+      </section>
+      <section className="result-container">
+        {/* <h3 id="resultText">{resultText}</h3> */}
+      </section>
+    </main>
+  );
+};
 
-export default App;
+export default RockPaperScissors;
